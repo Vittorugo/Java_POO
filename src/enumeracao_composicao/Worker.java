d367bacda93d65c549ed95b0f0d904c384876276
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-22 02:25:40 -0300

    Adicionado função income

--- src/enumeracao_composicao/Worker.java
+++ src/enumeracao_composicao/Worker.java
@@ -1,7 +1,9 @@
 package enumeracao_composicao;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
 
 public class Worker {
 	
@@ -9,17 +11,19 @@ public class Worker {
 	private WorkerLevel level;
 	private Double baseSalary;
 	
-	List<HourContract> contracts = new ArrayList<>();
+	private List<HourContract> contracts = new ArrayList<>();
+	private Departament nameDepartament;
 	
 	public Worker() {
 		
 	}
 	
-	public Worker( String name, WorkerLevel level, Double baseSalary) {
+	public Worker( String name, WorkerLevel level, Double baseSalary, Departament departament) {
 		
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
+		this.nameDepartament = departament;
 	}
 	
 	
@@ -47,6 +51,31 @@ public class Worker {
 			
 	}
 	
+	public Double income(int year, int month) {
+		
+		Calendar cal = Calendar.getInstance();
+		double total = this.baseSalary;
+		
+		for (HourContract c: contracts) {
+			
+			cal.setTime(c.getDate());
+						
+			int monthContract = 1 + cal.get(Calendar.MONTH);
+			int yearContract = cal.get(Calendar.YEAR);
+						
+			if(monthContract == month && yearContract == year) {
+				total += c.totalValue();
+			}
+		}
+		
+		// HourContract contractMonthYear = contracts.stream().filter( x -> (cal.get(Calendar.MONTH) == month)  && (cal.get(Calendar.YEAR) == year)).findAny().orElse(null);
+		// if (contractMonthYear != null){
+		// 		total += contractMonthYear.totalValue();
+		//}
+		return total;
+	}
+	
+	
 	
 	// Getters and Setters 
 	
@@ -62,6 +91,9 @@ public class Worker {
 		return this.baseSalary;
 	}
 	
+	public Departament getDepartament() {
+		return this.nameDepartament;
+	}
 	
 	public void setName(String newName) {
 		this.name = newName;

75cc3d0d4281a00fb2b2b305e720a4402b32c554
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-22 02:28:05 -0300

    Finalizando o exercício após inserir a interface para o usuario interagir

--- src/enumeracao_composicao/Main.java
+++ src/enumeracao_composicao/Main.java
@@ -1,8 +1,8 @@
 package enumeracao_composicao;
 
-import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,9 +12,12 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		Locale.setDefault(Locale.US);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		Scanner input = new Scanner(System.in);
-				
+		
+		System.out.print("Enter departament's name: ");
+		Departament departament = new Departament(input.nextLine());
+		
 		System.out.println("Worker data: ");
 		System.out.print("Name: ");
 		String name = input.nextLine();
@@ -29,7 +32,7 @@ public class Main {
 		Double salary = input.nextDouble();
 		
 		// instancia do trabalhador 
-		Worker worker = new Worker(name, l, salary);
+		Worker worker = new Worker(name, l, salary, departament);
 		
 		System.out.print("How many contracts to this worker? ");
 		int num = input.nextInt();
@@ -38,7 +41,7 @@ public class Main {
 			
 			input.nextLine();
 			
-			System.out.printf("Enter contract #%d data: ", i+1);
+			System.out.printf("Enter contract #%d data: %n", i+1);
 			System.out.print("Date (DD/MM/YYYY): ");
 			String date = input.next();
 			System.out.print("Value per hour: ");
@@ -46,22 +49,39 @@ public class Main {
 			System.out.print("Contract duration: ");
 			int duration = input.nextInt();
 			
-			
 			// convertendo string em Date ...
-			Date d = dateFormat.parse(date);
+			Date dateContract = dateFormat.parse(date);
+						
+			worker.addContract(new HourContract( (i+1), dateContract, value, duration));
 			
-			worker.addContract(new HourContract( (i+1), d, value, duration));
+			System.out.println();
 		}
 		
 		input.nextLine();
 		
 		
-		System.out.print("Enter ID contract remove: ");
-		worker.removeContract(input.nextInt());
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String[] monthYear = input.next().split("/");
 		
+		int month = Integer.parseInt(monthYear[0]);  
+		int year  = Integer.parseInt(monthYear[1]);
 		
+	
+		System.out.printf("Name: %s%n" , worker.getName());
+		System.out.printf("Departament: %s%n", worker.getDepartament().getName());
+		System.out.printf("Income for %d/%d: %.2f%n", month, year, worker.income(year, month));
+
+		System.out.print("Want to remove contract (y/n)? ");
+		char check = input.next().charAt(0);
 		
+		if ( check == 'y' || check == 'Y') {
+			System.out.print("Enter ID contract remove: ");
+			worker.removeContract(input.nextInt());
+		} else {
+			System.out.println("Good-Bye!");
+		}
 		
+				
 		input.close();
 		
 	}

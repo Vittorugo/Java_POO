9e034e6d31f178e9775b7890c0652cf24b4e134b
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-28 09:37:54 -0300

    Classe principal para interagir com o programa

--- src/excecoes2/Main.java
+++ src/excecoes2/Main.java
@@ -1,9 +1,46 @@
 package excecoes2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = input.nextInt();
+			System.out.print("Holder: ");
+			input.nextLine();
+			String name = input.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = input.nextDouble();
+			System.out.print("Withdrawl Limit: ");
+			double withdrawlimit = input.nextDouble();
+			
+			Account account = new Account(number, name, balance, withdrawlimit);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			double withdrawlValue = input.nextDouble();
+			
+			account.withdraw(withdrawlValue);
+			
+			System.out.print("New Balance: " + (account.getBalance() - withdrawlValue));
+			
+		} catch (WithDrawlException e) {
+			System.out.print("Error exception: " + e.getMessage());
+		}
+		catch (Exception e) {
+			System.out.println("Unexpected error");
+		}
+		
+		input.close();
 
 	}
 

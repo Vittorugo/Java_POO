8e81984ea5f07d90a3ef7146873d01a3895358de
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-28 20:29:54 -0300

    Adicinado comentários

--- jogo_xadrez/boardgame/Piece.java
+++ jogo_xadrez/boardgame/Piece.java
@@ -3,12 +3,12 @@ package boardgame;
 public class Piece {
 	
 	protected  Position position; // toda peça tem uma posição 
-	private Board board;
+	private Board board; // toda peça conhece o seu tabuleiro
 	
 	
 	public Piece(Board board) {
 		this.board = board;
-		this.position = null;
+		this.position = null; // toda peça começa com a posição nula
 	}
 
 

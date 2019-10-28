85b05c2a1d86a9ad6764ffe67a93fc909f8597f7
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-28 09:38:20 -0300

    Classe de exceções personalizadas

---
+++ src/excecoes2/WithDrawlException.java
@@ -0,0 +1,10 @@
+package excecoes2;
+
+public class WithDrawlException extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public WithDrawlException(String msg) {
+		super(msg);
+	}
+}
+	

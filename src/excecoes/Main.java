544ae3d55d52560c8d3d3d5c00eaf0f189eae8e4
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-27 22:51:42 -0300

    Classe principal de interação com o usuário

--- src/excecoes/Main.java
+++ src/excecoes/Main.java
@@ -1,10 +1,54 @@
 package excecoes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		Scanner input = new Scanner(System.in);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Room number: ");
+			int number = input.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = dateFormat.parse(input.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = dateFormat.parse(input.next());
+			
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = dateFormat.parse(input.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = dateFormat.parse(input.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		catch (ParseException e) {
+			System.out.println("Invalid date format");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		
+		
+		input.close();
+		
 	}
 
 }

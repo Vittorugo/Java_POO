1eeb460d1b86b2f31096f16ef3eafee15b23aca4
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-28 09:37:31 -0300

    Classe com informações da conta e opções de movimentação

--- src/excecoes2/Account.java
+++ src/excecoes2/Account.java
@@ -1,5 +1,67 @@
 package excecoes2;
 
 public class Account {
+	
+	private Integer number;
+	private String  holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+		
+	}
 
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	// Methods
+	
+	public void deposit(Double value) {
+		this.balance += value;
+	}
+	
+	public void withdraw(Double value) throws WithDrawlException {
+		
+		if (value > this.withdrawLimit) {
+			throw new WithDrawlException("Error removing. Your withdrawal limit is R$ " + String.format("%.2f", this.withdrawLimit) + ".");
+		}
+		
+		if (value > this.balance) {
+			throw new WithDrawlException("Insufficient funds!");
+		}
+		
+	}
+	
+	
+	// Getters and setters 
+	
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	
 }

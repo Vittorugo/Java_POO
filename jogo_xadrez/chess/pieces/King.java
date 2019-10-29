01061104e39a732927d2077d10af12baa01436a2
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-29 20:51:53 -0300

    definindo metodo com posições possiveis para mover o rei

--- jogo_xadrez/chess/pieces/King.java
+++ jogo_xadrez/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -9,15 +10,80 @@ public class King extends ChessPiece {
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
-	// Methods 
-	
+
+	// Methods
+
+	private boolean canMove(Position position) { // metodo que auxilia nos possiveis movimentos que o rei pode fazer
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || piece.getColor() != this.getColor();
+
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position position = new Position(0, 0);
+
+		// above
+
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// below
+
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// Left
+
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// Right
+
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}		
+		
+		// upper main diagonal
+
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// upper secondary diagonal
+
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// lower secondary diagonal
+
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// lower primary diagonal
+
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(position) && canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
 		return mat;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";

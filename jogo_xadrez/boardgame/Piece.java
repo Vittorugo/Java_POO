aa457bd2caad3dbafe2453c20e07bf6528e9572e
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-29 10:37:58 -0300

    Adicionando novos métodos

--- jogo_xadrez/boardgame/Piece.java
+++ jogo_xadrez/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected  Position position; // toda peça tem uma posição 
 	private Board board; // toda peça conhece o seu tabuleiro
@@ -11,7 +11,30 @@ public class Piece {
 		this.position = null; // toda peça começa com a posição nula
 	}
 
-
+	// Methods
+	
+	public abstract boolean[][] possibleMoves(); // metodo com possíveis movimentos das peças
+	
+	public boolean possibleMove(Position position) { // retorna uma matriz booleana com os possíveis movimentos das peças
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() { // percorre matriz e verifica se existe posição que a peça pode percorrer
+		boolean[][] mat = possibleMoves();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	// Getters and Setters 
+	
 	protected Board getBoard() { // apenas as peças poderão acessar este método
 		return board;
 	}

cb5b42f414c6ded0d81f8aab6ada18ca6144e3a8
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-28 21:03:34 -0300

    Adicionado tratamento de erros e métodos para verificar se as peças podem ser colocadas nos lugares escolhidos

--- jogo_xadrez/boardgame/Board.java
+++ jogo_xadrez/boardgame/Board.java
@@ -6,11 +6,10 @@ public class Board {
 	private Integer columns;
 	private Piece[][] pieces;
 	
-	public Board() {
-		
-	}
-	
 	public Board(Integer rows, Integer columns) {
+		if( rows < 1 || columns <1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -19,17 +18,42 @@ public class Board {
 	// Methods
 	
 	public Piece piece(int row, int column) { // retorna a peça localizada nas coordenadas passadas.
+		if(!positionExists(row,column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) { // sobrecarga que retorna a peça pela posição 
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece( Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		this.pieces[position.getRow()][position.getColumn()] = piece; // marcando a posição da peça no tabuleiro da peça
 		piece.position = position; // adicionando nova posição a peça
 	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < this.rows && column >= 0 && column < this.columns; // retorna se a linha e coluna estão dentro dos limites do tabuleiro.
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) { // verifica se tem uma peça na posição escolhida.
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;  
+	}
+	
 	
 	// Getters and Setters
 	
@@ -37,17 +61,9 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 		
 }

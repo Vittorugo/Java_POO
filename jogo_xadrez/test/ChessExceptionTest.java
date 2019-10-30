ef10bbc4b5a08ac1670a7a3b7970c81bc628c53c
Victtor Hugo <hgloureirocosta@gmail.com>
2019-10-30 11:53:27 -0300

    Classe para testes de excecoes

---
+++ jogo_xadrez/test/ChessExceptionTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPosition;
+
+class ChessExceptionTest {
+		
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void canNotChooseOffBoardPositionLowerLimit() {
+		
+		// Teste Limites inferiores 
+		int rowLower = 0; 
+		char columnLower = '`';
+		
+		ChessException thrown = assertThrows(ChessException.class, () -> new ChessPosition(columnLower,rowLower));
+		assertEquals("Error instantiating ChessPosition. Valid values are from a1 to h8.", thrown.getMessage());
+	}
+	
+	@Test
+	public void canNotChooseOffBoardPositionUpperLimit() {
+		
+		// Teste Limites superiores
+		int  rowUpper = 9;
+		char columnUpper = 'g';
+		
+		ChessException thrown = assertThrows(ChessException.class, () -> new ChessPosition(columnUpper, rowUpper));
+		assertEquals("Error instantiating ChessPosition. Valid values are from a1 to h8.", thrown.getMessage());
+	}
+	
+	@Test
+	public void breakIntoPositionOfTheSameTeamPiece() {
+				
+		// source
+		char sourceColumn = 'a';
+		int  sourceRow    = 8;
+		
+		// target
+		char targetColumn = 'e';
+		int  targetRow    = 8;
+		
+		ChessMatch moveChessMatch = new ChessMatch();
+				
+		ChessPosition sourPosition = new ChessPosition(sourceColumn, sourceRow);
+		ChessPosition targetPosition = new ChessPosition(targetColumn, targetRow);
+		
+		
+		ChessException thrown = assertThrows(ChessException.class, () -> moveChessMatch.performChessMove(sourPosition,targetPosition));
+		assertEquals("The chosen piece can't move to target position", thrown.getMessage());
+	}
+
+}

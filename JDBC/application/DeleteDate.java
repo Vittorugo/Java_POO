ca06687635d4c9d584ed951e9bdc11222ecf3158
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-11 11:02:36 -0300

    Deletar dados do banco

---
+++ JDBC/application/DeleteDate.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class DeleteDate {
+
+	public static void main(String[] args) {
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			
+			connection = DB.getConnection();
+			preparedStatement = connection.prepareStatement(
+												"DELETE FROM seller "
+												+ "WHERE "
+												+ ("DepartmentId = ?"));
+			preparedStatement.setInt(1, 2);
+			int rowsAffected = preparedStatement.executeUpdate();
+			
+			System.out.println(rowsAffected);
+			
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeConnection();
+		}
+	}
+
+}

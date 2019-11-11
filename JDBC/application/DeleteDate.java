bc73222332fbcdf3fc3aeb2269d176a549b3b0d0
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-11 11:12:34 -0300

    Delete MySQL

--- JDBC/application/DeleteDate.java
+++ JDBC/application/DeleteDate.java
@@ -10,22 +10,19 @@ import db.DbIntegrityException;
 public class DeleteDate {
 
 	public static void main(String[] args) {
-		
+
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
-		
+
 		try {
-			
+
 			connection = DB.getConnection();
-			preparedStatement = connection.prepareStatement(
-												"DELETE FROM seller "
-												+ "WHERE "
-												+ ("DepartmentId = ?"));
+			preparedStatement = connection.prepareStatement("DELETE FROM seller WHERE  (DepartmentId = ?)");
 			preparedStatement.setInt(1, 2);
 			int rowsAffected = preparedStatement.executeUpdate();
-			
+
 			System.out.println(rowsAffected);
-			
+
 		} catch (SQLException e) {
 			throw new DbIntegrityException(e.getMessage());
 		} finally {

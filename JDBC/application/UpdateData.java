2a51c295cb1e84a171210c29d28c949f21d10d58
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-11 11:13:16 -0300

    UPDATE MySQL

--- JDBC/application/UpdateData.java
+++ JDBC/application/UpdateData.java
@@ -9,26 +9,23 @@ import db.DB;
 import db.DbException;
 
 public class UpdateData {
-	
+
 	public static void main(String[] args) {
-		
+
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
-		
+
 		try {
 			// UPDATE com prepareStatement
 			connection = DB.getConnection();
-			preparedStatement = connection.prepareStatement(
-											"UPDATE seller"
-											+ " SET Name = ? "
-											+ " WHERE (Name = ?)");
-			
+			preparedStatement = connection.prepareStatement("UPDATE seller SET Name = ? WHERE (Name = ?)");
+
 			preparedStatement.setString(1, "Arthur");
 			preparedStatement.setString(2, "Eva");
-			
+
 			int rowsAffected = preparedStatement.executeUpdate();
 			System.out.println("Rows Affeted: " + rowsAffected);
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
